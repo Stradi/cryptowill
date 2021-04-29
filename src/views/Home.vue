23adc3c45365ce614bcab33f86d5ac4f31a1cdf5
Batın Evirgen <batinevirgen@gmail.com>
2021-04-29 21:00:35 +0300

    Changed layout of Home.vue view.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,22 @@
 <template>
-<div class="max-w-5xl mx-auto">
+<div class="max-w-6xl mx-auto">
   <div class="py-16 text-center">
-    <p>ready to create your CryptoWill?</p>
-    <h2 class="mb-4 text-4xl text-gray-200 font-bold">death could be close</h2>
-    <a href="#create" class="inline-block px-5 py-2 rounded-3xl border-2 border-gray-200 text-gray-200 transition duration-300 ease-out transform hover:-translate-y-1 hover:bg-white hover:bg-opacity-10">
+    <h2 class="mb-12 text-4xl md:text-6xl text-gray-200 font-bold">Don't lose your <span class="text-yellow-500">Crypto's</span> after you gone.</h2>
+    <a href="#create" class="inline-block px-5 py-2 rounded-3xl bg-yellow-700 text-gray-200 font-medium transition duration-300 ease-out transform hover:-translate-y-1 hover:bg-yellow-600">
       Create your CryptoWill
     </a>
   </div>
   <div class="flex justify-center">
-    <div class="w-5/12 max-h-44 mr-4 text-center bg-gray-800 rounded-3xl transition duration-300 ease-out transform rotate-2 hover:-rotate-1 bg-opacity-50">
-      <p class="text-gray-200 text-2xl py-4">What is CryptoWill?</p>
-      <p class="px-8">
-        CryptoWill will transfer your crypto funds to people you've selected after you die.<br>
-        So your funds will not disappear forever in blockchain.
-      </p>
-    </div>
-    <div class="w-5/12 ml-4">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, eius quis asperiores, ab sapiente aliquid quisquam, doloremque fuga eos cumque inventore reiciendis ipsum voluptate iste cupiditate dolorum cum consectetur repellat nulla. Numquam, nemo totam, provident ad tempore voluptate aspernatur quidem odio mollitia quo expedita suscipit dolore assumenda fugiat, iusto facere aliquam dolorem quas dolorum perspiciatis excepturi voluptates hic! Tempora et accusantium officia consectetur aliquam consequatur at, explicabo autem tempore id vel sequi nisi eius minus dolorem similique! Hic optio dolor sit numquam ut consequuntur, harum soluta nihil. Consectetur harum error deserunt maiores. Excepturi totam natus provident perspiciatis. Enim, labore eius.
+    <div class="max-w-2xl bg-gray-800 rounded-3xl bg-opacity-50 transition duration-300 ease-out transform hover:scale-105">
+      <div>
+        <p class="text-center text-gray-200 text-2xl py-4">What is CryptoWill?</p>
+        <p class="px-8 pb-4">
+          &nbsp;&nbsp;&nbsp;&nbsp;CryptoWill will <span class="text-yellow-500">transfer your cryptocoins</span> to the people you've selected, <span class="text-yellow-500">after you die.</span>
+          So your funds will not disappear forever in blockchain.<br><br>
+          &nbsp;&nbsp;&nbsp;&nbsp;You can also set <span class="text-yellow-500">percentage</span> for each person and <span class="text-yellow-500">leave a note</span> to them.
+          Your message will only be visible to person(s) you've chosen and only will be viewable after you <span class="text-yellow-500">(I am sorry)</span> died.
+        </p>
+      </div>
     </div>
   </div>
 </div>
