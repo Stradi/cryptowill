774a2e061b5c9fd8bb04ec56f42f99f8dd03fac1
Batın Evirgen <batinevirgen@gmail.com>
2021-05-03 14:10:53 +0300

    Added CTA button at the bottom of the screen and changed <a> tag to <router-link> tag.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,17 +4,18 @@
       Don't lose your <span class="text-yellow-500">Crypto's</span> after you
       gone.
     </h2>
-    <a
-      href="#create"
+    <router-link
+      :to="ctaUrl"
       class="inline-block px-5 py-2 rounded-3xl bg-yellow-700 text-gray-200 font-medium transition duration-300 ease-out transform hover:-translate-y-1 hover:bg-yellow-600"
     >
       Create your CryptoWill
-    </a>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-    name: "Hero"
+    name: "Hero",
+    props: ["ctaUrl"]
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <Hero class="max-w-6xl mx-auto"/>
+  <Hero class="max-w-6xl mx-auto" :ctaUrl="ctaUrl"/>
   <div class="py-8 md:py-16 bg-gray-800">
     <ImageTextCard image="/assets/grim-reaper.svg" title="What is CryptoWill?" animated bgColor="dark">
       &nbsp;&nbsp;&nbsp;&nbsp;CryptoWill will <span class="text-yellow-500">transfer your cryptocoins</span> to the people you've selected, <span class="text-yellow-500">after you die.</span>
@@ -16,6 +16,16 @@
   </ImageTextCard>
   <br>
   <Faq />
+  <br>
+  <div class="max-w-3xl mx-auto text-center py-16">
+    <p class="text-white text-3xl">Ready to create your CryptoWill?</p>
+    <router-link
+        :to="ctaUrl"
+        class="inline-block px-5 my-6 py-2 rounded-3xl bg-yellow-700 text-gray-200 font-medium transition duration-300 ease-out transform hover:-translate-y-1 hover:bg-yellow-600"
+      >
+        Create Now
+    </router-link>
+  </div>
   <Footer />
 </div>
 </template>
@@ -33,6 +43,11 @@ export default {
     ImageTextCard,
     Footer,
     Faq
+  },
+  data: () => {
+    return {
+      ctaUrl: "/my-will"
+    }
   }
 }
 </script>
