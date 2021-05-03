cc9cf31457444fbe1f43c4877a7bf797c4ad3034
Batın Evirgen <batinevirgen@gmail.com>
2021-05-03 14:27:24 +0300

    Added raw html for answers in FAQ section. This looks much better.

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="bg-gray-800 py-16">
     <div class="bg-gray-900 max-w-2xl mx-auto p-8 rounded-3xl">
-      <p class="pb-8 text-2xl text-center text-white">Frequently Asked Questions</p>
-      <FaqItem v-for="(item, idx) in items" v-bind:key="idx" :question="item.question" :answer="item.answer" />
+      <p class="pb-8 text-2xl text-center text-white font-medium">Frequently Asked Questions</p>
+      <FaqItem v-for="(item, idx) in items" v-bind:key="idx" :question="item.question">
+        <span v-html="item.answer"></span>
+      </FaqItem>
     </div>
   </div>
 </template>
@@ -18,12 +20,12 @@ export default {
   data: () => {
     return {
       items: [
-        { question: "What is CryptoWill", answer: "CryptoWill is a service that transfer your cryptocoins to specified person(s) after you died."},
-        { question: "How it works?", answer: "CryptoWill lives in blockchain, means it'll live forever. So anyone can create a will anytime. When you create a will, you are actually giving permission to CryptoWill to transfer your funds after you died."},
-        { question: "How CryptoWill knows that I am dead?", answer: "When 50% of your payees confirms that you are dead, after 60 days, they will be granted to withdraw the amount of cryptocoins person specified for them."},
-        { question: "What happens if my payees falsely confirms that I am dead?", answer: "When 50% of your payees confirms the death, they should wait 60 days in order to withdraw the cryptocoins. In that 60 days, you can always cancel the withdraw operation. After 60 days, it'll be impossible to cancel withdraw operation."},
-        { question: "Is CryptoWill free?", answer: "CryptoWill is not free. Because of the server and domain expenses. Will creation costs about 0.05BNB."},
-        { question: "What is some features of CryptoWill?", answer: "You can set percentage for each coin for each payee. For example, you may want to send %X of CoinA to PersonA and %(100-X) of CoinA to PersonB. This process can be done for each cryptocoin and for each person. You can also set messages for each payee. The messages you've set can be only viewed 60 days after the confirmation." }
+        { question: "What is CryptoWill", answer: "CryptoWill is a service that <span class='text-yellow-500'>transfer your cryptocoins</span> to specified person(s) after you died."},
+        { question: "How it works?", answer: "CryptoWill lives in blockchain, means it'll live forever. So anyone can <span class='text-yellow-500'>create a will</span> anytime. When you create a will, you are actually giving permission to CryptoWill to transfer your funds after you died."},
+        { question: "How CryptoWill knows that I am dead?", answer: "When <span class='text-yellow-500'>50%</span> of your payees confirms that you are dead, <span class='text-yellow-500'>after 60 days</span>, they will be granted to withdraw the amount of cryptocoins person specified for them."},
+        { question: "What happens if my payees falsely confirms that I am dead?", answer: "When 50% of your payees confirms the death, they will wait <span class='text-yellow-500'>60 days</span> in order to withdraw the cryptocoins. In that 60 days, you can always <span class='text-yellow-500'>cancel the withdraw operation.</span> After 60 days, it'll be <span class='text-yellow-500'>impossible to cancel</span> withdraw operation."},
+        { question: "Is CryptoWill free?", answer: "CryptoWill is <span class='text-yellow-500'>not free.</span> Because of the <span class='text-yellow-500'>server and domain expenses.</span> Will creation costs about <span class='text-yellow-500'>0.05BNB.</span>"},
+        { question: "What is some features of CryptoWill?", answer: "You can set percentage <span class='text-yellow-500'>for each coin for each payee</span>. For example, you may want to send %X of CoinA to PersonA and %(100-X) of CoinA to PersonB. This process can be done for <span class='text-yellow-500'>each cryptocoin</span> and for <span class='text-yellow-500'>each person</span>. You can also set <span class='text-yellow-500'>messages for each payee</span>. The messages you've set can be <span class='text-yellow-500'>only viewed 60 days after the confirmation.</span>" }
       ]
     }
   }
--- src/components/FaqItem.vue
+++ src/components/FaqItem.vue
@@ -1,13 +1,13 @@
 <template>
   <details class="mb-4 bg-gray-800 rounded-3xl p-4 bg-opacity-50 transition duration-100 ease-out hover:bg-opacity-80">
-    <summary class="text-white select-none">{{question}}</summary>
-    <div class="px-4 py-2">{{answer}}</div>
+    <summary class="text-white select-none font-medium">{{question}}</summary>
+    <slot class="px-4 py-2"></slot>
   </details>
 </template>
 
 <script>
 export default {
   name: "FaqItem",
-  props: ["question", "answer"]
+  props: ["question"]
 }
 </script>
