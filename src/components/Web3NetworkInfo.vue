9a4d4e7d037aaee14abfa683d0cf4bb1d001220a
Batın Evirgen <batinevirgen@gmail.com>
2021-05-04 12:57:19 +0300

    Changed design of Web3NetworkInfo component for mobile devices.

--- src/components/Web3NetworkInfo.vue
+++ src/components/Web3NetworkInfo.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="p-4 bg-gray-800 bg-opacity-50">
     <div v-if="isConnected" class="flex justify-between">
-      <p>Connected to <span class="font-medium">{{ networkName }}</span> with <a :href="explorerURI"><span class="font-medium text-yellow-500 transition duration-100 ease-out hover:text-yellow-600">{{ web3.selectedAccount }}</span></a></p>
-      <ConnectWeb3Button text="Reconnect" />
+      <p class="hidden md:block">Connected to <span class="font-medium">{{ networkName }}</span> with <a :href="explorerURI"><span class="font-medium text-yellow-500 transition duration-100 ease-out hover:text-yellow-600">{{ web3.selectedAccount }}</span></a></p>
+      <ConnectWeb3Button text="Reconnect" class="hidden md:block" />
+
+      <details class="md:hidden">
+        <summary>Network Information</summary>
+        <div>
+          <p>{{ networkName }}</p>
+          <a :href="explorerURI"><p class="font-medium text-yellow-500 transition duration-100 ease-out hover:text-yellow-600">{{ web3.selectedAccount }}</p></a>
+          <ConnectWeb3Button text="Reconnect" />
+        </div>
+      </details>
     </div>
     <div v-else class="flex justify-end">
       <ConnectWeb3Button text="Connect" />
