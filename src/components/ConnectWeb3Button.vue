4cd8a6382ac8ae9cc76c0368a1a80f265ef1318d
Batın Evirgen <batinevirgen@gmail.com>
2021-05-04 12:37:50 +0300

    Created ConnectWeb3Button component.

---
+++ src/components/ConnectWeb3Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <a href="#" @click="connect" class="font-medium text-yellow-500 transition duration-300 ease-out hover:text-yellow-600">{{ text }}</a>
+</template>
+
+<script>
+export default {
+  name: "ConnectWeb3Button",
+  props: ["text"],
+  methods: {
+    connect() {
+      this.$store.dispatch("registerWeb3");
+    }
+  },
+}
+</script>
--- src/components/Web3NetworkInfo.vue
+++ src/components/Web3NetworkInfo.vue
@@ -2,21 +2,21 @@
   <div class="p-4 bg-gray-800 bg-opacity-50">
     <div v-if="isConnected" class="flex justify-between">
       <p>Connected to <span class="font-medium">{{ networkName }}</span> with <a :href="explorerURI"><span class="font-medium text-yellow-500 transition duration-100 ease-out hover:text-yellow-600">{{ web3.selectedAccount }}</span></a></p>
-      <a href="#" @click="connect" class="font-medium text-yellow-500">Reconnect</a>
+      <ConnectWeb3Button text="Reconnect" />
     </div>
     <div v-else class="flex justify-end">
-      <a href="#" @click="connect" class="font-medium text-yellow-500">Connect to Web3 Provider</a>
+      <ConnectWeb3Button text="Connect" />
     </div>
   </div>
 </template>
 
 <script>
+import ConnectWeb3Button from "@/components/ConnectWeb3Button.vue";
+
 export default {
   name: "Web3NetworkInfo",
-  methods: {
-    connect() {
-      this.$store.dispatch("registerWeb3");
-    }
+  components: {
+    ConnectWeb3Button
   },
   computed: {
     web3() {
