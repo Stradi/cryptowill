229cc5df12ab36d7c8f0762374e0a6f4390e9d1c
Batin EVIRGEN <batinevirgen@gmail.com>
2021-05-13 17:16:37 +0300

    Aligned Modal component vertically. Updated ViewPayeeModal.vue and created token.js file in order to convert token addresses from-to short names, like BNB or ETH.

--- src/components/Dashboard/PayeeList/ViewPayeeModal.vue
+++ src/components/Dashboard/PayeeList/ViewPayeeModal.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="">
+  <div>
     <p><span class="font-medium">Alias:</span> {{ item.alias || "Charmander" }}</p>
     <p><span class="font-medium">Address:</span> {{ item.address }}</p>
   </div>
   <div class="mb-2">
     <label for="message" class="font-medium block pb-2">Message:</label>
-    <textarea v-model="message" id="message" name="message" rows="4" cols="60" :placeholder="placeholder" :class="isMessageChanged ? 'ring-2 ring-yellow-500' : ''" class="text-black p-2 rounded-2xl focus:outline-none resize-none"></textarea>
-    <label for="shares" class="font-medium block py-2">Shares</label>
-    <div v-for="share, idx in this.shares" :key="idx" class="flex flex-col pb-4">
-      <p>{{ share.address }}</p>
-      <div>
-        <input v-model="this.shares[idx].percentage" v-on:input="percentageChanged(idx)" type="range" min="0" max="100" class="w-11/12 align-middle" />
-        <span class="pl-2">{{ this.shares[idx].percentage }}</span>
-      </div>
+    <textarea v-model="message" id="message" name="message" rows="4" cols="70" :placeholder="placeholder" :class="isMessageChanged ? 'ring-2 ring-yellow-500' : ''" class="text-black p-2 rounded-2xl focus:outline-none resize-none"></textarea>
+    <div class="flex justify-between py-2">
+      <p for="shares" class="font-medium">Shares</p>
+      <a href="#" class="font-medium text-yellow-500 transition ease-out duration-200 hover:text-yellow-400">Add new share</a>
     </div>
+    <table class="table-fixed">
+      <thead>
+        <th class="w-2/12 text-center">Token</th>
+        <th class="w-8/12 text-center"></th>
+        <th class="w-2/12 text-center">Percentage</th>
+      </thead>
+      <tbody>
+        <tr v-for="share, idx in this.shares" :key="share.address" class="text-center">
+          <td class="py-2">{{ share.name }}</td>
+          <td>
+            <input v-model="share.share" v-on:input="percentageChanged(idx)" type="range" min="0" max="100" class="w-full align-middle" />
+          </td>
+          <td>
+            <span class="text-center">{{ share.share }}</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <div class="flex justify-evenly text-center">
       <a href="#" @click="save" :class="!this.isAnythingChanged ? 'hover:cursor-not-allowed hover:bg-yellow-800 bg-yellow-800' : ''" class="py-2 w-1/5 font-medium bg-yellow-700 rounded-3xl transition duration-300 ease-out hover:bg-yellow-600 hover:shadow-lg">Save</a>
       <a href="#" @click="this.$emit('close')" class="py-2 font-medium bg-red-700 rounded-3xl transition duration-300 ease-out hover:bg-red-600 w-1/5">Cancel</a>
@@ -22,9 +36,12 @@
 </template>
 
 <script>
+import { TOKENS } from "@/utils/token.js";
+
 export default {
   name: "EditPayeeModal",
   props: ["item"],
+  emits: ["close"],
   data: function() {
     return {
       message: this.item.message,
@@ -44,18 +61,35 @@ export default {
   },
   methods: {
     percentageChanged(idx) {
-      this.shares[idx].percentage = Math.max(0, Math.min(Number.parseInt(this.shares[idx].percentage), this.shares[idx].percentageLeft));
-      this.changedShares[idx] = this.shares[idx].percentage !== Number.parseInt(this.$props.item.shares[idx].share);
+      let initialShare = Number.parseInt(this.item.shares[idx].share);
+      this.shares[idx].share = this.clamp(this.shares[idx].share, 0, initialShare + this.shares[idx].percentageLeft);
+      this.changedShares[idx] = this.shares[idx].share !== initialShare;
+    },
+    clamp(val, min, max) {
+      return Math.min(Math.max(val, min), max);
     }
   },
   beforeMount() {
     for(let i = 0; i < this.shares.length; i++) {
       this.shares[i].percentageLeft = this.$store.state.contract.approvedCoins[i].percentageLeft;
-      if(this.shares[i].percentage === undefined) {
-        this.shares[i].percentage = 0;
+      
+      if(TOKENS["TESTNET"][this.shares[i].address] === undefined) {
+        this.shares[i].name = this.shares[i].address;
+      } else {
+        this.shares[i].name = TOKENS["TESTNET"][this.shares[i].address];
+      }
+
+      if(this.shares[i].share === undefined) {
+        this.shares[i].share = 0;
         this.$props.item.shares[i].share = 0;
+      } else {
+        this.shares[i].share = Number.parseInt(this.shares[i].share);
       }
     }
   }
 }
 </script>
+
+<style>
+
+</style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -18,6 +18,7 @@
 
 <script>
 export default {
-  name: "Modal"
+  name: "Modal",
+  emits: ["close"]
 }
 </script>
