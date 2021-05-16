27c7d565216e0ae0e65db7ba3bddb041e04ec83b
Batin EVIRGEN <batinevirgen@gmail.com>
2021-05-16 14:14:04 +0300

    Changed style of SearchDropdown a bit.

--- src/components/Common/SearchDropdown.vue
+++ src/components/Common/SearchDropdown.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
+  <div class="max-w-max mx-auto">
     <input ref="searchBox" v-model="term" @input="isSelected=false" @click="isSelected=false" type="text" :placeholder="placeholder" :class="isSelected ? 'ring-2 ring-yellow-500' : ''" class="text-black p-2 rounded-xl outline-none">
     <div v-if="!isSelected" class="p-2 bg-white text-black mt-2 rounded-xl">
       <ul v-if="filteredItems.length !== 0">
-        <li v-for="item in filteredItems.slice(0,5)" :key="item" @click="selected(item)" class="cursor-pointer px-3 py-1 my-1 select-none rounded-3xl transition duration-200 ease-out hover:bg-yellow-200">
+        <li v-for="item in filteredItems.slice(0,5)" :key="item" @click="selected(item)" class="cursor-pointer px-2 py-1 select-none rounded-xl transition duration-200 ease-out hover:bg-yellow-200">
           {{ item }}
         </li>
       </ul>
       <ul v-else>
-        <li class="px-3 py-1 my-1 select-none">Token not found</li>
+        <li class="px-2 select-none">Token not found</li>
       </ul>
     </div>
   </div>
