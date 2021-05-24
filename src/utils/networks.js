8f0ef1bb6f76f338e26314ec2baf4fe3d3f0c2c1
Batin EVIRGEN <batinevirgen@gmail.com>
2021-05-24 14:04:58 +0300

    Updated token addresses and contract address deployed on test network.

--- src/utils/networks.js
+++ src/utils/networks.js
@@ -4,5 +4,5 @@ export const NETWORKS = {
   "4": { name: "Rinkeby Test Network", explorer: "https://rinkeby.etherscan.io/address/{0}", contractAddress: "0x0000000000000000000000000000000000000000" },
   "42": { name: "Kovan Test Network", explorer: "https://kovan.etherscan.io/address/{0}", contractAddress: "0x0000000000000000000000000000000000000000" },
   "56": { name: "Binance Smart Chain", explorer: "https://bscscan.com/address/{0}", contractAddress: "0x0000000000000000000000000000000000000000" },
-  "97": { name: "Binance Smart Chain Test Network", explorer: "https://testnet.bscscan.com/address/{0}", contractAddress: "0x7DbE5faCFe13dCD9dB817a9Cab49a3AcC8ce1Efe" },
+  "97": { name: "Binance Smart Chain Test Network", explorer: "https://testnet.bscscan.com/address/{0}", contractAddress: "0x928606802acC1bd56dF76205103a07C2c735aB86" },
 }
