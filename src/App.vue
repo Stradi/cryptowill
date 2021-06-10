11b0e81a1eca6f0600a6e487fc128f09dea0b2ba
Batin EVIRGEN <batinevirgen@gmail.com>
2021-06-10 15:54:41 +0300

    Forgot to move change network notification to Dashboard.

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,6 @@
     <Web3NetworkInfo />
     <NavigationBar />
     <router-view />
-    <Modal v-if="!isNetworkCorrect && this.$store.state.web3.isConnected">
-      <template v-slot:header>
-        Wrong Network
-      </template>
-      <template v-slot:body>
-        <div class="p-4 text-center">
-          Please change the connected network to BSC and reconnect.<br>
-          <ConnectWeb3Button text="Reconnect" />
-        </div>
-      </template>
-    </Modal>
     <Footer />
   </div>
 </template>
@@ -22,25 +11,13 @@
 import NavigationBar from "@/components/Common/NavigationBar.vue";
 import Footer from "@/components/Common/Footer.vue";
 import Web3NetworkInfo from "@/components/Common/Web3NetworkInfo.vue";
-import Modal from "@/components/Common/Modal.vue";
-import ConnectWeb3Button from "@/components/Common/ConnectWeb3Button.vue";
 
 export default {
   name: "App",
   components: {
     NavigationBar,
     Footer,
-    Web3NetworkInfo,
-    Modal,
-    ConnectWeb3Button
-  },
-  computed: {
-    connectedNetworkId() {
-      return this.$store.state.web3.networkId;
-    },
-    isNetworkCorrect() {
-      return this.connectedNetworkId === 97;
-    }
+    Web3NetworkInfo
   },
   async beforeCreate() {
     await this.$store.dispatch("web3/connect");
@@ -48,7 +25,6 @@ export default {
     await this.$store.dispatch("contract/getApprovedCoins");
     await this.$store.dispatch("contract/getPayees");
     await this.$store.dispatch("contract/getPayers");
-
   }
 }
 </script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -11,6 +11,17 @@
       <div class="py-4 "><Withdraw /></div>
     </div>
   </div>
+  <Modal v-if="!isNetworkCorrect && this.$store.state.web3.isConnected">
+      <template v-slot:header>
+        Wrong Network
+      </template>
+      <template v-slot:body>
+        <div class="p-4 text-center">
+          Please change the connected network to BSC and reconnect.<br>
+          <ConnectWeb3Button text="Reconnect" />
+        </div>
+      </template>
+    </Modal>
 </template>
 
 <script>
@@ -19,13 +30,26 @@ import Confirm from "@/components/Dashboard/Confirm/Confirm.vue";
 import Withdraw from "@/components/Dashboard/Withdraw/Withdraw.vue";
 import MyPayers from "@/components/Dashboard/MyPayers/MyPayers.vue";
 
+import Modal from "@/components/Common/Modal.vue";
+import ConnectWeb3Button from "@/components/Common/ConnectWeb3Button.vue";
+
 export default {
   name: "Dashboard",
   components: {
     PayeeList,
     Confirm,
     Withdraw,
-    MyPayers
+    MyPayers,
+    Modal,
+    ConnectWeb3Button
+  },
+  computed: {
+    connectedNetworkId() {
+      return this.$store.state.web3.networkId;
+    },
+    isNetworkCorrect() {
+      return this.connectedNetworkId === 97;
+    }
   }
 }
 </script>
